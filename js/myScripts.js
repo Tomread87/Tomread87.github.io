9d2a4b3f691fe1893e87ea45cf41eb138a811bca
Tomread87 <ivoteruggi@hotmail.com>
2021-03-19 15:17:09 +0000

    latest

--- js/myScripts.js
+++ js/myScripts.js
@@ -204,6 +204,7 @@ function LightDark(){
         $(".after-description").addClass("dark-after-description")
         $("#dark-mode img").css("height","24px")
         $("#light-mode img").css("height","16px")
+        $("#nav-menu").addClass("darkmode")
 
     }
     if (mode == "light") {
